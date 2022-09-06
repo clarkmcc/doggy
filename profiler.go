3594e180eeb48ef4fdc0813624d6ce21c14d6afa
Clark McCauley <clarkmccauley@gmail.com>
2022-09-06 14:07:05 -0600

    Updated profiler options

--- profiler.go
+++ profiler.go
@@ -8,22 +8,24 @@ import (
 
 // StartProfiler starts the profiler with the provided parameters. The service name is required,
 // but the service version and environment are not required.
-func StartProfiler(serviceName, serviceVersion, env string, options ...profiler.Option) error {
+func StartProfiler(serviceName, serviceVersion string, options ...profiler.Option) error {
+	// Maintain our own opts list so that we can put all our default options in front of
+	// anything provided by the options parameter. This will ensure that anything provided
+	// in the options parameter will override the manually set service name, version, or env
+	var opts []profiler.Option
 	if len(serviceName) == 0 {
 		return fmt.Errorf("service name required by profiler")
 	}
 	if addr := os.Getenv("DOGSTATSD_APM_ADDR"); len(addr) > 0 {
-		options = append(options, profiler.WithAgentAddr(addr))
+		opts = append(opts, profiler.WithAgentAddr(addr))
 	}
 	if len(serviceVersion) > 0 {
-		options = append(options, profiler.WithVersion(serviceVersion))
+		opts = append(opts, profiler.WithVersion(serviceVersion))
 	}
-	if len(env) > 0 {
-		options = append(options, profiler.WithEnv(env))
-	} else if env = os.Getenv("ENV"); len(env) > 0 {
-		options = append(options, profiler.WithEnv(env))
+	if env := os.Getenv("ENV"); len(env) > 0 {
+		opts = append(opts, profiler.WithEnv(env))
 	}
-	return profiler.Start(options...)
+	return profiler.Start(append(opts, options...)...)
 }
 
 // StopProfiler stops the profiler
